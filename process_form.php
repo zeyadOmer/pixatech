e1ad70c2f67f3abe9b89d425b0f5bb9f95f5cf6a
zeyad <zeyad@fahad.com>
2023-09-14 11:50:27 +0400

    header

--- process_form.php
+++ process_form.php
@@ -25,6 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Send the email
     if (mail($to, $subject, $message, $headers)) {
         echo "<script type='text/javascript'>alert('thank you for contacing pixatech');</script>";
+        header('Location: index.html');
     } else {
         echo "Oops! gg Something went wrong and we couldn't send your message.";
     }
